9f0df80fdd703e2783331b86eed53d191f90e3b4
Dr-rer-nat-Who <115470853+Dr-rer-nat-Who@users.noreply.github.com>
2025-06-18 12:57:11 +0200

    Use TimeSeriesSplit for LightGBM evaluation

--- python/models/__init__.py
+++ python/models/__init__.py
@@ -1,7 +1,5 @@
 """Collection of simple model implementations used in Optuna studies."""
 
-from . import lightgbm, catboost, tabnet, prophet, n_linear, lstm, tft, autoformer, informer, patchtst, timesnet, finrl_ppo
-
 __all__ = [
     "lightgbm",
     "catboost",
